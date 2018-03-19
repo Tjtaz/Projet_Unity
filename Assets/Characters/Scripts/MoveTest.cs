6a945d9a8fa9a24fa3102dd12e9cfe327dc92ac2
MSI\Tjtaz <>
2018-03-19 13:56:54 +0100

    Player 1 code fini

--- Assets/Characters/Scripts/MoveTest.cs
+++ Assets/Characters/Scripts/MoveTest.cs
@@ -7,8 +7,10 @@ public class MoveTest : MonoBehaviour {
 
     public float RunSpeed;
     public float SideSpeed;
-    public Vector3 JumpSpeed;
+    public float JumpSpeed;
     private int Rail;
+    private BoxCollider characterCollider;
+
 
     public Rigidbody rb;
 
@@ -17,7 +19,7 @@ public class MoveTest : MonoBehaviour {
     {
         rb = GetComponent<Rigidbody>();
         Assert.IsNotNull(rb);
-        
+        characterCollider = gameObject.GetComponent<BoxCollider>();
     }
     private void Update()
     {
@@ -27,7 +29,7 @@ public class MoveTest : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+
         //Rail
         Rail = 0;
         if (Input.GetKey(KeyCode.D))
@@ -36,48 +38,60 @@ public class MoveTest : MonoBehaviour {
         }
 
         if (Input.GetKey(KeyCode.Q))
-        { 
+        {
             Rail = -1;
         }
-
+        
         switch (Rail)
         {
             case 1:
                 {
                     //transform.position = new Vector3(2, transform.position.y, transform.position.z);
                     Vector3 speed = Vector3.zero;
-                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(2, transform.position.y, transform.position.z), ref speed, 0.05f);
+                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(2, transform.position.y, transform.position.z), ref speed, 0.035f);
                 }
                 break;
             case -1:
                 {
                     //transform.position = new Vector3(-2, transform.position.y, transform.position.z);
                     Vector3 speed = Vector3.zero;
-                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(-2, transform.position.y, transform.position.z), ref speed, 0.05f);
+                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(-2, transform.position.y, transform.position.z), ref speed, 0.035f);
                 }
                 break;
-            default :
+            default:
                 {
                     //transform.position = new Vector3(0, transform.position.y, transform.position.z);
                     Vector3 speed = Vector3.zero;
-                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(0, transform.position.y, transform.position.z), ref speed, 0.05f);
+                    transform.position = Vector3.SmoothDamp(transform.position, new Vector3(0, transform.position.y, transform.position.z), ref speed, 0.035f);
                 }
                 break;
 
         }
         
-        //Jump
 
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (rb.velocity.y >= -0.05 && rb.velocity.y <= 0.05)
         {
-            //Préparation saut
-            Vector3 v = gameObject.GetComponent<Rigidbody>().velocity = JumpSpeed;
-            v.y = JumpSpeed.y;
-            //Saut
-            gameObject.GetComponent<Rigidbody>().velocity = JumpSpeed;
-            //Raycast
-            
-        }
+            //Jump
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                rb.AddForce(transform.up * JumpSpeed, ForceMode.Impulse);
+                characterCollider.size = new Vector3(characterCollider.size.x, 2.0f, characterCollider.size.z);
+                characterCollider.center = new Vector3(characterCollider.center.x, 0.5f, characterCollider.center.z);
+            }
+
+
+            //Slide
+            else if (Input.GetKey(KeyCode.S))
+            {
+                characterCollider.size = new Vector3(characterCollider.size.x, 1f, characterCollider.size.z);
+                characterCollider.center = new Vector3(characterCollider.center.x, 0f, characterCollider.center.z);
+            }
+            else
+            {
+                characterCollider.size = new Vector3(characterCollider.size.x, 2.0f, characterCollider.size.z);
+                characterCollider.center = new Vector3(characterCollider.center.x, 0.5f, characterCollider.center.z);
+            }
+        }        
     }
 
 }
