141d1b19037809b7fc6c45568e79609689ac86ad
MSI\mondo <mondohamoeva@hotmail.fr>
2018-03-19 14:04:46 +0100

    new version spawn cube

---
+++ Assets/Player 2/Script/Move_j2.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Move_j2 : MonoBehaviour
+{
+
+
+    public float speed = 15f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Player 2/Script/SpawnCubes.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject CubeObstacle;
+    public GameObject CubeObstacles;
+    public GameObject SpawnCube;
+    public GameObject SpawnCubeL;
+    public GameObject SpawnCubeR;
+    public GameObject SpawnCubeUP;
+    public GameObject SpawnCubeDwn;
+    public float coolDown = 1;
+    public float coolDownTimer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coolDownTimer >0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
+        if (coolDownTimer < 0)
+        {
+            coolDownTimer = 0;
+        }
+
+        if (Input.GetKeyDown("l") && coolDownTimer ==0)
+        {
+            SpawnCubeMiddle();
+            coolDownTimer = coolDown;
+        }
+        if (Input.GetKeyDown("k") && coolDownTimer == 0)
+        {
+            SpawnCubeLeft();
+            coolDownTimer = coolDown;
+        }
+        if (Input.GetKeyDown("m") && coolDownTimer == 0)
+        {
+            SpawnCubeRight();
+            coolDownTimer = coolDown;
+        }
+        if (Input.GetKeyDown("o") && coolDownTimer == 0)
+        {
+            SpawnCubeUp();
+            coolDownTimer = coolDown;
+        }
+        if (Input.GetKeyDown("p") && coolDownTimer == 0)
+        {
+            SpawnCubeDown();
+            coolDownTimer = coolDown;
+        }
+       
+    }   
+
+    void SpawnCubeMiddle()
+    {
+        Instantiate(CubeObstacle, SpawnCube.transform.position, SpawnCube.transform.rotation);
+        
+    }
+    void SpawnCubeLeft()
+    {
+        Instantiate(CubeObstacle, SpawnCubeL.transform.position, SpawnCubeL.transform.rotation);
+        
+    }
+    void SpawnCubeRight()
+    {
+        Instantiate(CubeObstacle, SpawnCubeR.transform.position, SpawnCubeR.transform.rotation);
+       
+    }
+    void SpawnCubeUp()
+    {
+        Instantiate(CubeObstacles, SpawnCubeUP.transform.position, SpawnCubeUP.transform.rotation);
+    }
+    void SpawnCubeDown()
+    {
+        Instantiate(CubeObstacles, SpawnCubeDwn.transform.position, SpawnCubeDwn.transform.rotation);
+    }
+}
