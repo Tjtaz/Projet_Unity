8a701e86fa2d083f7fe6a40e9e71ea57463255f2
MSI\Tjtaz <>
2018-03-22 00:53:08 +0100

    Hop hop hop y'a tout là dedans

---
+++ Assets/Menu/Menu_script/Menu_Defeat.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Defeat : MonoBehaviour
+{
+
+    public void Return_Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+
+    }
+}
+
+
---
+++ Assets/Menu/Menu_script/Menu_Victory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Victory : MonoBehaviour
+{
+
+    public void Return_Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+
+    }
+}
+
+
+
